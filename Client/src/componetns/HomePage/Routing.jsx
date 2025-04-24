223fc9e7b3e30fdbcdd8245e3375e58b3bb2a613
Bibek1604 <npbibek94@gmail.com>
2025-04-24 12:36:28 +0545

    basic setu[p

---
+++ Client/src/componetns/HomePage/Routing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import First from './First'
+import Second from './second'
+import Third from './Third'
+
+function Routing() {
+  return (
+    <div>
+        <First />
+        <Second />
+        <Third />
+    </div>
+  )
+}
+
+export default Routing
