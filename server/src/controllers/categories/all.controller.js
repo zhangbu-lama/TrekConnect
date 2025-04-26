565522fd4c96140c69a78e20cd2b413d6bc46cb0
Nishan Bista <lw.nishan@gmail.com>
2025-04-26 12:26:19 +0545

    feat: get category by id

--- server/src/controllers/categories/all.controller.js
+++ server/src/controllers/categories/all.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler, SuccessResponse } from "../../lib/index.js";
 import { Categorie } from "../../models/category.model.js";
-export const getAllCategories = asyncHandler(async (req, res) => {
+export const getAllCategories = asyncHandler(async (_, res) => {
     const categories = await Categorie.find();
 
     return res.status(200).json(
