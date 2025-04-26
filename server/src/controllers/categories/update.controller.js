8f566b5d76055edec39294ce89554e439181f46f
Nishan Bista <lw.nishan@gmail.com>
2025-04-26 12:50:42 +0545

    feat: update category

--- server/src/controllers/categories/update.controller.js
+++ server/src/controllers/categories/update.controller.js
@@ -1,8 +1,41 @@
-import { asyncHandler, SuccessResponse } from "../../lib/index.js";
+import { isValidObjectId } from "mongoose";
+import {
+    asyncHandler,
+    ErrorResponse,
+    SuccessResponse,
+} from "../../lib/index.js";
+import { Categorie } from "../../models/category.model.js";
 export const updateCategory = asyncHandler(async (req, res) => {
+    const catId = req.params.id;
+    const name = req.body?.name ?? "";
+    const description = req.body?.description ?? "";
+    const isImageUpdated = req.body?.isImageUpdated ?? false;
+    const image = req.file ?? null;
+
+    if (!isValidObjectId(catId)) {
+        throw new ErrorResponse(400, 6000, "invalid category id");
+    }
+
+    if (!name) {
+        throw new ErrorResponse(400, 6000, "updated name is required");
+    }
+
+    if (!description) {
+        throw new ErrorResponse(400, 6000, "updated description is required");
+    }
+
+    if (!image && isImageUpdated) {
+        throw new ErrorResponse(400, 6000, "image is required");
+    }
+
+    const category = await Categorie.findById(catId);
+
+    category.name = name;
+    category.description = description;
+    category.image = isImageUpdated ? image : category.image;
+    await category.save();
+
     return res
         .status(200)
-        .json(
-            new SuccessResponse(200, "desired action done succesfully", null),
-        );
+        .json(new SuccessResponse(200, "category updated", category));
 });
--- server/src/routes/categories.route.js
+++ server/src/routes/categories.route.js
@@ -13,5 +13,9 @@ export const categoriesRouter = express.Router();
 categoriesRouter.get("/all", getAllCategories);
 categoriesRouter.get("/:id", getCategorieById);
 categoriesRouter.post("/add", upload.single("category_image"), addCategory);
-categoriesRouter.patch("/update/:id", updateCategory);
+categoriesRouter.patch(
+    "/update/:id",
+    upload.single("category_image"),
+    updateCategory,
+);
 categoriesRouter.delete("/delete/:id", deleteCategory);
