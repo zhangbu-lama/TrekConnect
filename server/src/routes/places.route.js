8184ece5d3235a001f40b46162ced4ee5ce71ffa
Nishan Bista <lw.nishan@gmail.com>
2025-04-26 13:45:44 +0545

    place,categories done

--- server/src/routes/places.route.js
+++ server/src/routes/places.route.js
@@ -14,5 +14,5 @@ export const placesRouter = express.Router();
 placesRouter.get("/all", upload.single("place_image"), getAllPlaces);
 placesRouter.get("/:id", getPlaceById);
 placesRouter.post("/add", addPlace);
-placesRouter.patch("/update/:id", updatePlace);
+placesRouter.put("/update/:id", upload.single("place_image"), updatePlace);
 placesRouter.delete("/delete/:id", deletePlace);
