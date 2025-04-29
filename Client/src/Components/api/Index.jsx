f3a048f9fdab7a8b111e0410bdad246247033b78
Bibek1604 <npbibek94@gmail.com>
2025-04-29 21:48:24 +0545

    update the add category page from admin panel

--- Client/src/Components/api/Index.jsx
+++ Client/src/Components/api/Index.jsx
@@ -1,17 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-
-
   baseURL: 'http://localhost:8000/api',
 });
-
-
 axiosInstance.interceptors.request.use(config => {
   return config;
 });
-
-
 export default axiosInstance;
 
 
