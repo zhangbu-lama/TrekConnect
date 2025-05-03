64963e4565b33c9fdae10fe7733cb258c789f38d
Bibek1604 <npbibek94@gmail.com>
2025-05-03 09:38:06 +0545

    updated the categoty field

--- Client/src/Components/UserDashboard/Productbooked.jsx
+++ Client/src/Components/UserDashboard/Productbooked.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Sidebar from './UserSidebar'
 
 function Productbooked() {
   return (
+    <>
+    <Sidebar />
     <div>Productbooked</div>
+    </>
   )
 }
 
--- Client/src/Components/UserDashboard/placebook.jsx
+++ Client/src/Components/UserDashboard/placebook.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Sidebar from './UserSidebar'
 
 function Placebook() {
   return (
+    <>
+    <Sidebar />
     <div>placebook</div>
+    </>
+    
   )
 }
 
