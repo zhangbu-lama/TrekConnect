9ab5e07a173a8fffd33d15a6bf20242585e2e0f5
zhangbu-lama <zhangbulama5@gmail.com>
2025-05-08 20:31:14 +0545

    all path update4d

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+//import "./App.css";
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import useAuthStore from "./Components/Store/AuthStore.jsx";
@@ -40,7 +40,6 @@ import UserLoginPage from "./Components/UserAuth/Userlogin.jsx";
 import UserRegisterPage from "./Components/UserAuth/UserRegister.jsx";
 import ProtectedRoute from "./Components/Auth/ProtectedRoute.jsx";
 
-
 //payment integration
 
 import Sucess from "./Components/PaymentIntegration/Success.jsx";
@@ -78,7 +77,6 @@ function App() {
                 <Route path="/failed" element={<Failed />} />
                 <Route path="/esewapayment" element={<ESewaPayment />} />
 
-
                 <Route path="/" element={<Routing />} />
                 <Route path="/trek" element={<Reusable />} />
                 <Route path="/details/:id" element={<Reusabledetails />} />
@@ -91,7 +89,6 @@ function App() {
                 <Route path="/admin/register" element={<RegisterPage />} />
                 <Route path="/userlogin" element={<UserLoginPage />} />
                 <Route path="/userregister" element={<UserRegisterPage />} />
-
 
                 {/* Admin Routes */}
                 <Route
@@ -209,108 +206,17 @@ function App() {
                         </ProtectedRoute>
                     }
                 />
+                <Route
+                    path="/bookproduct"
+                    element={
+                        <ProtectedRoute requiredRole="user">
+                            <ProductBooking />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </Router>
     );
 }
 
 export default App;
-
-// import "./App.css";
-// import React, { useEffect } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import useAuthStore from "./Components/Store/AuthStore.jsx";
-//
-// // Navbar Components
-// import MainNavbar from "./Components/HomePage/Navbar.jsx";
-// import UserNavbar from "./Components/HomePage/UserNavbar.jsx";
-// import AdminNavbar from "./Components/HomePage/AdminNavbar.jsx";
-//
-// // Public Pages
-// import Routing from "./Components/HomePage/Routing.jsx";
-// import Reusable from "./Components/Secondpage/Reusable.jsx";
-// import Reusabledetails from "./Components/Detailspage/Reusabledetails.jsx";
-// import BookingFormPage from "./Components/Detailspage/Bookingform.jsx";
-// import Bouldering from "./Components/Bouldering/Bouldering.jsx";
-// import Down from "./Components/HomePage/Down.jsx";
-//
-// // Admin Authentication
-// import LoginPage from "./Components/Auth/Login.jsx";
-// import RegisterPage from "./Components/Auth/Register.jsx";
-//
-// // Admin Panel Pages
-// import Dashboard from "./Components/AdminPanel/Dashboard.jsx";
-// import AddPage from "./Components/AdminPanel/AddPlacePage.jsx";
-// import AddDetails from "./Components/AdminPanel/AddDetails.jsx";
-// import ShowBookings from "./Components/AdminPanel/Placebooking.jsx";
-// import AddCategory from "./Components/AdminPanel/Category.jsx";
-// import Location from "./Components/AdminPanel/Location.jsx";
-// import AdminPanel from "./Components/AdminPanel/Addproduct.jsx";
-// import ShowBooking from "./Components/AdminPanel/ShowBooking.jsx";
-// import ProductBooking from "./Components/Bouldering/ProductBooking.jsx";
-//
-// // User Pages
-// import UserLayout from "./Components/UserDashboard/UserLayout.jsx";
-// import UserSidebar from "./Components/UserDashboard/UserSidebar.jsx";
-// import Placebook from "./Components/UserDashboard/placebook.jsx";
-// import Productbooked from "./Components/UserDashboard/Productbooked.jsx";
-// import UserLoginPage from "./Components/UserAuth/Userlogin.jsx";
-// import UserRegisterPage from "./Components/UserAuth/UserRegister.jsx";
-//
-// function App() {
-//   const { user, adminToken, initializing, initializeAuth } = useAuthStore();
-//
-//   useEffect(() => {
-//     initializeAuth();
-//   }, [initializeAuth]);
-//
-//   if (initializing) {
-//     return (
-//       <div className="min-h-screen flex items-center justify-center bg-gray-100">
-//         <div className="text-gray-800 text-lg font-semibold animate-pulse">
-//           Loading...
-//         </div>
-//       </div>
-//     );
-//   }
-//
-//   return (
-//     <Router>
-//       {adminToken ? <AdminNavbar /> : user ? <UserNavbar /> : <MainNavbar />}
-//       <Routes>
-//         <Route path="/" element={<Routing />} />
-//         <Route path="/trek" element={<Reusable />} />
-//         <Route path="/details/:id" element={<Reusabledetails />} />
-//         <Route path="/bookingform" element={<BookingFormPage />} />
-//         <Route path="/bouldering" element={<Bouldering />} />
-//         <Route path="/bookproduct" element={<ProductBooking />} />
-//         <Route path="/bookproduct/:id" element={<ProductBooking />} />
-//         <Route path="/down" element={<Down />} />
-//
-//         <Route path="/admin/login" element={<LoginPage />} />
-//         <Route path="/admin/register" element={<RegisterPage />} />
-//         <Route path="/userlogin" element={<UserLoginPage />} />
-//         <Route path="/userregister" element={<UserRegisterPage />} />
-//
-//         {/* Admin Routes (Now Public) */}
-//         <Route path="/admin" element={<Dashboard />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-//         <Route path="/addproduct" element={<AdminPanel />} />
-//         <Route path="/productbooking" element={<ShowBooking />} />
-//         <Route path="/page" element={<AddPage />} />
-//         <Route path="/add-details" element={<AddDetails />} />
-//         <Route path="/add-category" element={<AddCategory />} />
-//         <Route path="/addlocation" element={<Location />} />
-//         <Route path="/show-bookings" element={<ShowBookings />} />
-//
-//         {/* User Routes (Now Public) */}
-//         <Route path="/placebooked" element={<Placebook />} />
-//         <Route path="/productbooked" element={<Productbooked />} />
-//         <Route path="/user" element={<UserSidebar />} />
-//         <Route path="/userlayout" element={<UserLayout />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-//
-// export default App;
